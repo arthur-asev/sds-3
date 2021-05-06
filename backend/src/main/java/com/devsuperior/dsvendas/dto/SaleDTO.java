fb1f2ab1205f39cc7c27f4537107b69747379a15
Arthur Rodrigues <arthurginho02@gmail.com>
2021-05-06 12:10:32 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -0,0 +1,93 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.time.LocalDate;
+
+import com.devsuperior.dsvendas.entities.Sale;
+
+public class SaleDTO  {
+	private Long Id;
+	private Integer visited;
+	private Integer deals;
+	private Double amount ;
+	private LocalDate date;
+	private SellerDTO seller;
+	
+	public SaleDTO() {
+		
+	}
+
+	public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDTO seller) {
+		super();
+		Id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+		this.seller = seller;
+	}
+	
+	public SaleDTO(Sale entity) {
+		super();
+		Id = entity.getId();
+		this.visited = entity.getVisited();
+		this.deals = entity.getDeals();
+		this.amount = entity.getAmount();
+		this.date = entity.getDate();
+		this.seller = new SellerDTO(entity.getSeller());
+
+		
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public SellerDTO getSeller() {
+		return seller;
+	}
+
+	public void setSeller(SellerDTO seller) {
+		this.seller = seller;
+	}
+	
+	
+	
+	
+	
+}
